ebb4621ea9ee1b90ce3eb0077f3253a9aff7c7cf
Amir <amirmohammad.mehrabi@gmail.com>
2025-05-12 10:17:23 +0330

    clrek added

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,7 +1,12 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-export default function layout({
+export default async function layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const user = await currentUser();
+  const isAdmin = user?.privateMetadata?.isAdmin;
+  if (!isAdmin) redirect('/');
   return <div>{children}</div>;
 }
